77ac15352184de4ccf2a68d967ebc84a085e531f
Dudchenko Olesya <dudchenkoleska@gmail.com>
2022-10-07 02:11:48 +0300

    Исправлен баг с делением на 0

---
+++ TrainingProject_DivOperation/div_operation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+
+    setlocale(LC_ALL, "russian");
+
+    int a, b;
+    float div;
+
+    scanf_s("%d", &a);
+    scanf_s("%d", &b);
+
+    if (b == 0) {
+        printf("На 0 делить нельзя\n");
+    }
+   
+    system("pause");
+    return 0;
+}
